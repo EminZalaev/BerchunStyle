b24038df8f2d86f38540eb8fc5a8991f1d8823b8
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-29 10:54:15 +0300

    FIX

---
+++ main/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	lambda := 3.0
+	mu := 1.0
+	n := 2
+	N := 5
+	//K := make([]float64, N)
+
+	Kcalc := Kfunc(n, N, lambda, mu)
+	fmt.Println("K: ", Kcalc)
+
+	Po := PoFunc(Kcalc, N)
+	fmt.Println("Po: ", Po)
+
+	MQ := MfromQ(Kcalc, n, N, Po)
+	fmt.Println("M(Q): ", MQ)
+
+	PQ := PfromQ(Kcalc, n, N, Po)
+	fmt.Println("P(Q): ", PQ)
+
+	MN := MfromN(Kcalc, n, N, Po)
+	fmt.Println("M(N): ", MN)
+
+	Kzn := MN / float64(n)
+	fmt.Println("Kzn: ", Kzn)
+}
+
+func Kfunc(n int, N int, lambda float64, mu float64) []float64 {
+	K := make([]float64, N)
+	K[0] = (float64(N) * lambda) / mu
+
+	for i := 1; i < N; i++ {
+		if i < n {
+			K[i] = K[i-1] * ((float64(N-i) * lambda) / (float64(i+1) * mu))
+		} else {
+			K[i] = K[i-1] * ((float64(N-i) * lambda) / (float64(n) * mu))
+		}
+	}
+	return K
+}
+
+func PoFunc(K []float64, N int) float64 {
+	var sum, Po float64
+
+	for i := 0; i < N; i++ {
+		sum += K[i]
+	}
+	Po = float64(1) / sum
+
+	return Po
+}
+
+func MfromQ(K []float64, n int, N int, Po float64) float64 {
+	var M_N float64
+	var j int
+
+	for i := n; i < N; i++ {
+		M_N += float64(j) * K[i]
+		j++
+	}
+	M_N = Po * M_N
+
+	return M_N
+}
+
+func PfromQ(K []float64, n int, N int, Po float64) float64 {
+	var M_N float64
+
+	for i := n; i < N; i++ {
+		M_N += K[i]
+	}
+	M_N = Po * M_N
+
+	return M_N
+}
+
+func MfromN(K []float64, n int, N int, Po float64) float64 {
+	var M_N float64
+
+	for i := 0; i < N; i++ {
+		if i < n {
+			M_N += float64(i+1) * K[i]
+		} else {
+			M_N += float64(n) * K[i]
+		}
+	}
+	M_N = Po * M_N
+
+	return M_N
+}
--- refactor/main.go
+++ refactor/main.go
@@ -14,12 +14,11 @@ func main() {
 	lambda := 3.0
 	mu := 1.0
 	//part 1.1
-	fmt.Println("\nlambda =", lambda, "\nmu = ", mu)
 	for n := 1; n <= 4; n++ {
 		K := make([]float64, n)
 
 		Kcomp := Kfunc(lambda, mu, n, K)
-		fmt.Println("\nK1: ", Kcomp)
+		fmt.Println("K1: ", Kcomp)
 
 		Po := PoCalc(K, n)
 		fmt.Println("Po: ", Po)
@@ -38,9 +37,9 @@ func main() {
 	Q := 2
 	n := 4
 	K := make([]float64, n+Q)
-	fmt.Println("\nlambda =", lambda, "\nmu = ", mu, "\nn =", n, "\nQ =", Q)
+
 	K2 := Kfunc2(n, Q, lambda, mu, K)
-	fmt.Println("\nK2: ", K2)
+	fmt.Println("K2: ", K2)
 
 	Po2 := PoCalc(K2, n+Q)
 	fmt.Println("Po2: ", Po2)
@@ -54,7 +53,7 @@ func main() {
 	Kz2 := MfromN2(K2, Po2, n, Q) / float64(n)
 	fmt.Println("Kz2: ", Kz2)
 
-	PQ2 := PfromQ2(Po2, K2, lambda, mu, n, Q)
+	PQ2 := P_Q2(Po2, K2, lambda, mu, n, Q)
 	fmt.Println("P(Q)2: ", PQ2)
 
 	M_Q2 := MfromQ(Po2, K2, n, Q)
@@ -65,10 +64,9 @@ func main() {
 
 	// part 1.3
 	K3 := make([]float64, n)
-	fmt.Println("\nlambda =", lambda, "\nmu = ", mu, "\nQ =", Q)
 
 	Kcomp3 := Kfunc(lambda, mu, n, K3)
-	fmt.Println("\nK3: ", Kcomp3)
+	fmt.Println("K3: ", Kcomp3)
 
 	Po3 := PoCalc3(K3, n, lambda, mu)
 	fmt.Println("Po3: ", Po3)
@@ -79,10 +77,10 @@ func main() {
 	Kz3 := M_N3 / float64(n)
 	fmt.Println("Kz3: ", Kz3)
 
-	PQ3 := PfromQ3(K3, n, lambda, mu, Po3)
+	PQ3 := PQ_3(K3, n, lambda, mu, Po3)
 	fmt.Println("P(Q)3: ", PQ3)
 
-	MQ3 := MfromQ3(K3, n, lambda, mu, Po3)
+	MQ3 := MQ_3(K3, n, lambda, mu, Po3)
 	fmt.Println("M(Q)3: ", MQ3)
 
 	K_ZQ3 := MQ3 / float64(n)
@@ -92,10 +90,8 @@ func main() {
 	v := 1.0
 	K4 := make([]float64, n)
 
-	fmt.Println("\nlambda =", lambda, "\nmu = ", mu, "\nQ =", Q, "\nV =", v)
-
 	Kcomp4 := Kfunc(lambda, mu, n, K4)
-	fmt.Println("\nK4: ", Kcomp4)
+	fmt.Println("K4: ", Kcomp4)
 
 	Po4, ii := PoCalc4(K4, n, lambda, mu, v)
 	fmt.Println("Po4: ", Po4, "\nii: ", ii)
@@ -106,7 +102,7 @@ func main() {
 	Kz4 := M_N4 / float64(n)
 	fmt.Println("K(Z)4: ", Kz4)
 
-	PQ4 := PfromQ4(K4, n, Po4, lambda, mu, v, ii)
+	PQ4 := PQ_4(K4, n, Po4, lambda, mu, v, ii)
 	fmt.Println("P(Q)4: ", PQ4)
 
 	MQ4 := MfromQ4(K, n, Po4, lambda, mu, v, ii)
@@ -114,7 +110,6 @@ func main() {
 
 	Kzq4 := MQ4 / float64(n)
 	fmt.Println("Kzq4: ", Kzq4)
-	fmt.Println()
 }
 
 func Kfunc(lambda float64, mu float64, n int, K []float64) []float64 {
@@ -125,6 +120,28 @@ func Kfunc(lambda float64, mu float64, n int, K []float64) []float64 {
 	}
 
 	return K
+}
+
+func PoCalc(K []float64, n int) float64 {
+	var sum float64
+
+	for i := 0; i < n; i++ {
+		sum += K[i]
+	}
+
+	Po := 1 / (1 + sum)
+
+	return Po
+}
+
+func MfromN(K []float64, Po float64, n int) float64 {
+	var sum float64
+
+	for i := 0; i < n; i++ {
+		sum += K[i] * float64(i+1)
+	}
+
+	return Po * sum
 }
 
 func Kfunc2(n int, Q int, lambda float64, mu float64, K []float64) []float64 {
@@ -145,16 +162,40 @@ func Kfunc2(n int, Q int, lambda float64, mu float64, K []float64) []float64 {
 	return K
 }
 
-func PoCalc(K []float64, n int) float64 {
+func MfromN2(K []float64, Po float64, n int, Q int) float64 {
 	var sum float64
 
-	for i := 0; i < n; i++ {
-		sum += K[i]
+	for i := 0; i < n+Q; i++ {
+		if i < n {
+			sum += K[i] * float64(i+1)
+		} else {
+			sum += K[i] * float64(n)
+		}
 	}
 
-	Po := 1 / (1 + sum)
+	return Po * sum
+}
 
-	return Po
+func P_Q2(Po float64, K []float64, lambda float64, mu float64, n int, Q int) float64 {
+	i := n
+	var sum float64
+
+	for i = n; i < n+Q; i++ {
+		sum += math.Pow((lambda / (float64(n) * mu)), float64(i-n+1))
+	}
+
+	return K[n-1] * Po * sum
+}
+
+func MfromQ(Po float64, K []float64, n int, Q int) float64 {
+	i := n
+	var sum float64
+
+	for i = n; i < n+Q; i++ {
+		sum += float64(i-n+1) * K[i]
+	}
+
+	return Po * sum
 }
 
 func PoCalc3(K []float64, n int, lambda float64, mu float64) float64 {
@@ -164,7 +205,25 @@ func PoCalc3(K []float64, n int, lambda float64, mu float64) float64 {
 		sum += K[i]
 	}
 
-	return float64(1) / (sum + (K[n-1] * (geta(lambda, n, mu) / (float64(1) - geta(lambda, n, mu)))))
+	return float64(1) / (sum + (K[n-1] * (lambda / (float64(n) * mu) / (float64(1) - (lambda / float64(n) * mu)))))
+}
+
+func MformN3(K []float64, n int, Po float64, lambda float64, mu float64) float64 {
+	var sum float64
+
+	for i := 0; i < n; i++ {
+		sum += float64(i+1) * K[i]
+	}
+
+	return Po * (sum + float64(n)*(K[n-1])*(lambda/(float64(n)*mu)/(float64(1)-(lambda/float64(n)*mu))))
+}
+
+func PQ_3(K []float64, n int, lambda float64, mu float64, Po float64) float64 {
+	return K[n-1] * Po * (lambda / (float64(n) * mu) / (float64(1) - (lambda / float64(n) * mu)))
+}
+
+func MQ_3(K []float64, n int, lambda float64, mu float64, Po float64) float64 {
+	return K[n-1] * Po * K[n-1] * Po * (lambda / (float64(n) * mu) / math.Pow((float64(1)-(lambda/float64(n)*mu)), 2.0))
 }
 
 func PoCalc4(K []float64, n int, lambda float64, mu float64, v float64) (float64, int) {
@@ -182,9 +241,9 @@ func PoCalc4(K []float64, n int, lambda float64, mu float64, v float64) (float64
 
 	for i = 1; i > 0; i++ {
 		if i == 1 {
-			A = getA(lambda, mu, n, i, v)
+			A = (lambda / (float64(n)*mu + float64(i)*v))
 		} else {
-			A = A * getA(lambda, mu, n, i, v)
+			A = A * (lambda / (float64(n)*mu + float64(i)*v))
 		}
 
 		sum += K[n-1] * A
@@ -200,40 +259,6 @@ func PoCalc4(K []float64, n int, lambda float64, mu float64, v float64) (float64
 	return Po, ii
 }
 
-func MfromN(K []float64, Po float64, n int) float64 {
-	var sum float64
-
-	for i := 0; i < n; i++ {
-		sum += K[i] * float64(i+1)
-	}
-
-	return Po * sum
-}
-
-func MfromN2(K []float64, Po float64, n int, Q int) float64 {
-	var sum float64
-
-	for i := 0; i < n+Q; i++ {
-		if i < n {
-			sum += K[i] * float64(i+1)
-		} else {
-			sum += K[i] * float64(n)
-		}
-	}
-
-	return Po * sum
-}
-
-func MformN3(K []float64, n int, Po float64, lambda float64, mu float64) float64 {
-	var sum float64
-
-	for i := 0; i < n; i++ {
-		sum += float64(i+1) * K[i]
-	}
-
-	return Po * (sum + float64(n)*(K[n-1])*(geta(lambda, n, mu)/(float64(1)-(geta(lambda, n, mu)))))
-}
-
 func MfromN4(K []float64, n int, Po float64, lambda float64, mu float64, v float64, ii int) float64 {
 	var M_N, A, MN float64
 	var i int
@@ -244,9 +269,9 @@ func MfromN4(K []float64, n int, Po float64, lambda float64, mu float64, v float
 
 	for i = 1; i < ii; i++ {
 		if i == 1 {
-			A = getA(lambda, mu, n, i, v)
+			A = (lambda / (float64(n)*mu + float64(i)*v))
 		} else {
-			A = A * getA(lambda, mu, n, i, v)
+			A = A * (lambda / (float64(n)*mu + float64(i)*v))
 		}
 
 		M_N += float64(n) * K[n-1] * A
@@ -256,29 +281,14 @@ func MfromN4(K []float64, n int, Po float64, lambda float64, mu float64, v float
 	return MN
 }
 
-func PfromQ2(Po float64, K []float64, lambda float64, mu float64, n int, Q int) float64 {
-	i := n
-	var sum float64
-
-	for i = n; i < n+Q; i++ {
-		sum += math.Pow(geta(lambda, n, mu), float64(i-n+1))
-	}
-
-	return K[n-1] * Po * sum
-}
-
-func PfromQ3(K []float64, n int, lambda float64, mu float64, Po float64) float64 {
-	return K[n-1] * Po * (geta(lambda, n, mu) / (float64(1) - geta(lambda, n, mu)))
-}
-
-func PfromQ4(K []float64, n int, Po float64, lambda float64, mu float64, v float64, ii int) float64 {
+func PQ_4(K []float64, n int, Po float64, lambda float64, mu float64, v float64, ii int) float64 {
 	var P_Q, PQ, A float64
 
 	for i := 1; i < ii; i++ {
 		if i == 1 {
-			A = getA(lambda, mu, n, i, v)
+			A = (lambda / (float64(n)*mu + float64(i)*v))
 		} else {
-			A = A * getA(lambda, mu, n, i, v)
+			A = A * (lambda / (float64(n)*mu + float64(i)*v))
 		}
 
 		P_Q += A
@@ -288,29 +298,14 @@ func PfromQ4(K []float64, n int, Po float64, lambda float64, mu float64, v float
 	return PQ
 }
 
-func MfromQ(Po float64, K []float64, n int, Q int) float64 {
-	i := n
-	var sum float64
-
-	for i = n; i < n+Q; i++ {
-		sum += float64(i-n+1) * K[i]
-	}
-
-	return Po * sum
-}
-
-func MfromQ3(K []float64, n int, lambda float64, mu float64, Po float64) float64 {
-	return K[n-1] * Po * K[n-1] * Po * (geta(lambda, n, mu) / math.Pow((float64(1)-geta(lambda, n, mu)), 2.0))
-}
-
 func MfromQ4(K []float64, n int, Po float64, lambda float64, mu float64, v float64, ii int) float64 {
 	var M_Q, MQ, A float64
 
 	for i := 1; i < ii; i++ {
 		if i == 1 {
-			A = getA(lambda, mu, n, i, v)
+			A = (lambda / (float64(n)*mu + float64(i)*v))
 		} else {
-			A = A * getA(lambda, mu, n, i, v)
+			A = A * (lambda / (float64(n)*mu + float64(i)*v))
 		}
 
 		M_Q += float64(i) * A
